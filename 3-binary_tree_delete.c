4dae873ad902e9bf01c62d60d68565b6239b1b63
kevxcyj <kevcyjunior@gmail.com>
2024-04-15 04:02:53 +0000

    Added number 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,19 +1,15 @@
 #include "binary_trees.h"
 /**
- * 
- * 
- * 
- * 
- * 
- * 
+ * binary_tree_delete - Deletes binary tree
+ * @tree: Pointer
 */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-
-
-
-
-
-    
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
 }
