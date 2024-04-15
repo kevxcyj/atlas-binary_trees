3204d6f33dde6dd0501c295b2ea11706d07a861f
kevxcyj <kevcyjunior@gmail.com>
2024-04-15 04:22:38 +0000

    Added number 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - Uses post order traversal
+ * @tree: Pointer to tree
+ * @func: Pointer to function
+*/
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+}
