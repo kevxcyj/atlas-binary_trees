57c545162e71686c72eae0fc82ba5c57569c8ef5
kevxcyj <kevcyjunior@gmail.com>
2024-04-15 04:47:26 +0000

    Added height function to bttm

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,7 +9,29 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree)
-		return (binary_tree_balance(tree->left) - binary_tree_height(tree->right));
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
+	return (0);
+}
+
+#include "binary_trees.h"
+/**
+ * binary_tree_height - Measures the height of node
+ * @tree: Pointer to tree
+ *
+ * Return: Height of node or 0
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t lf = 0;
+		size_t rt = 0;
+
+		lf = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		rt = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((lf > rt) ? lf : rt);
+	}
 	return (0);
 }
