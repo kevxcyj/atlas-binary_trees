12f7ce4e8d904bdcb534d3c015d5173dc11be9bc
kevxcyj <kevcyjunior@gmail.com>
2024-04-15 04:58:14 +0000

    Changed order of functions in 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,19 +1,5 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_full - Checks if binary tree is full
- * @tree: Pointer to tree
- *
- * Return: 0 or 1
-*/
-
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	return (binary_tree_recursive(tree));
-}
-
-/**
  * binary_tree_recursive - Checks if tree is recursive
  * @tree: Pointer to tree
  *
@@ -30,4 +16,21 @@ int binary_tree_recursive(const binary_tree_t *tree)
 			return (0);
 	}
 	return (1);
+}
+
+
+
+
+/**
+ * binary_tree_is_full - Checks if binary tree is full
+ * @tree: Pointer to tree
+ *
+ * Return: 0 or 1
+*/
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (binary_tree_recursive(tree));
 }
