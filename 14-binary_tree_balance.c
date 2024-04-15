0335386d955d54d6cb6242ff75a709cee9275e7c
kevxcyj <kevcyjunior@gmail.com>
2024-04-15 04:49:36 +0000

    Changed output to 1

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,7 +14,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 	return (0);
 }
 
-#include "binary_trees.h"
 /**
  * binary_tree_height - Measures the height of node
  * @tree: Pointer to tree
@@ -29,8 +28,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		size_t lf = 0;
 		size_t rt = 0;
 
-		lf = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		rt = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		lf = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		rt = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		return ((lf > rt) ? lf : rt);
 	}
 	return (0);
